5689c9343ddf54a3f10e6648ea765f1c82909391
Double0negative <drew@superleague.com>
2017-09-19 14:39:15 -0700

    mysql

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 var express = require("express");
+
+const server = express();
+
+
+server.get("/", function(req, res) {
+  res.send("<h1>hello world</h1>");
+});
+
+var id = 0;
+server.get("/api/server", function(req, res) {
+  id++;
+  var player =  {
+    id: id,
+    name: req.query.first,
+  }
+  res.send(player)
+})
+
+server.listen("5000", function() {
+  console.log("server is started");
+})
